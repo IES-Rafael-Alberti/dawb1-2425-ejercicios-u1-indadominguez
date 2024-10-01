51e961c19da6835b161a44fa6f96401efa9212c3
dhindalecio <dhindalecio@iesrafaelalberti.es>
2024-10-01 20:26:11 +0200

    Nueva actualización

--- src/ej04.py
+++ src/ej04.py
@@ -6,3 +6,13 @@ temperatura_fahrenheits = (temperatura_celcius * 9/5) + 32
 
 
 print(f"La temperatura en grados Fahrenheit es: {temperatura_fahrenheits:.2f}°F")
+
+
+
+def conversion_a_celcius():
+
+
+
+def main():
+    print(conversion_a_celcius)
+
