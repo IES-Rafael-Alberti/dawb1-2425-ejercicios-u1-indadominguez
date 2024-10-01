99c2af4431d6e4d1a80112ba99f0c7501f05411b
dhindalecio <dhindalecio@iesrafaelalberti.es>
2024-10-01 17:02:04 +0200

    Cambios

---
+++ src/ej06.py
@@ -0,0 +1,25 @@
+#Sexto ejercicio 
+
+importe_final = float(input("introduce el importe final del artículo "))
+
+tipo_iva = 0.10
+
+importe_sin_iva = importe_final / (1 + tipo_iva)
+
+iva_pagado = importe_final - importe_sin_iva
+
+print(f"Importe sin IVA: {importe_sin_iva:.2f} €")
+print(f"IVA pagado: {iva_pagado:.2f} €")
+
+
+
+
+
+
+   
+
+
+
+
+
+
---
+++ src/ej07.py
@@ -0,0 +1,12 @@
+#Séptimo ejercicio
+
+num1 = float(input("Introduce el primer número: "))
+
+num2 = float(input("Introduce el segundo número: "))
+
+num3 = float(input("Introduce el tercer número: "))
+
+suma = num1 + num2 + num3
+
+print("La suma de los 3 números es:", suma)
+
