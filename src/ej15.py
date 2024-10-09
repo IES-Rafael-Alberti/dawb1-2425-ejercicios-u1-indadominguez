e4b88532319363fd1f8d337cdb87892f6a057c5a
dhindalecio <dhindalecio@iesrafaelalberti.es>
2024-10-09 12:08:33 +0200

    NUveos avances

--- src/ej15.py
+++ src/ej15.py
@@ -1,1 +1,13 @@
 # ejercicio 15
+
+cuenta_ahorros = float(input("Introduce la cantidad de dinero que tienes: "))
+
+intereses = 0.04
+
+ahorros_primer_año = cuenta_ahorros * (1 + intereses)
+ahorros_segundo_año = ahorros_primer_año * (1 + intereses)
+ahorros_tercer_año = ahorros_segundo_año * (1 + intereses)
+
+print(f"Ahorros tras el primer año: {ahorros_primer_año:.2f} ")
+print(f"Ahorros tras el segundo año: {ahorros_segundo_año:.2f} ")
+print(f"Ahorros tras el tercer año: {ahorros_tercer_año:.2f} ")
